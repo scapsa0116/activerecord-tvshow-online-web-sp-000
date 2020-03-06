382c859b0eb85bf217e5ab581aa562dc5a5d5b2b
Stela Capsa <stela.capsa@gmail.com>
2020-03-06 17:18:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,6 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    add_column :shows, :season, :string
-     add_column :season
+   add_column :shows, :season, :string
+   
   end
 end
