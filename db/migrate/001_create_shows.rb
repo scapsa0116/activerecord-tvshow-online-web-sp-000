9400f5796c57af507a69e9ddf42bfce6a6638cf0
Stela Capsa <stela.capsa@gmail.com>
2020-03-06 17:31:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration[5.2]
 def change
-  create_table :shows do |s|
+    create_table :shows do |s|
     s.string :name 
     s.string :network
     s.string :day
